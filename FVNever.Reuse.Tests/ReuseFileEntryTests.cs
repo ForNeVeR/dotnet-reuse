f3dcc2d75b701e61389fbf653c68865224a5e8b6
Friedrich von Never <friedrich@fornever.me>
2026-09-07 00:28:35 +0200

    CopyrightNotice: infrastructure to parse year ranges

--- FVNever.Reuse.Tests/ReuseFileEntryTests.cs
+++ FVNever.Reuse.Tests/ReuseFileEntryTests.cs
@@ -3,6 +3,7 @@ using TruePath.SystemIo;
 
 namespace FVNever.Reuse.Tests;
 
+// REUSE-IgnoreStart
 public class ReuseFileEntryTests
 {
     [Theory]
@@ -14,19 +15,8 @@ public class ReuseFileEntryTests
     [InlineData("Copyright ©", "")]
     public async Task NameIsParsedCorrectly(string fileContent, string holderName)
     {
-        var file = Temporary.CreateTempFile();
-        try
-        {
-            await file.WriteAllTextAsync(fileContent, TestContext.Current.CancellationToken);
-            var content = await ReuseFileEntry.ReadFromFile(file);
-            Assert.NotNull(content);
-            var notice = content.CopyrightNotices.Single();
-            Assert.Equal(holderName, notice.HolderName);
-        }
-        finally
-        {
-            file.Delete();
-        }
+        var notice = await ParseCopyrightNotice(fileContent);
+        Assert.Equal(holderName, notice.HolderName);
     }
 
     [Theory]
@@ -34,16 +24,49 @@ public class ReuseFileEntryTests
     [InlineData("Copyright")]
     public async Task InvalidCopyrightIsNotParsed(string fileContent)
     {
+        var content = ParseFileEntry(fileContent);
+        Assert.Null(content);
+    }
+
+    [Fact]
+    public async Task YearsGetParsedCorrectly()
+    {
+        var copyright = await ParseCopyrightNotice("Copyright 2022-2023, 2025 Friedrich von Never");
+        Assert.Equal([
+            new CopyrightNotice.YearItem.YearRange(2022, 2023),
+            new CopyrightNotice.YearItem.SingleYear(2025)
+        ], copyright.Years);
+    }
+
+    [Fact]
+    public async Task YearsFailOnBrokenSeparators()
+    {
+        var copyright = await ParseCopyrightNotice("Copyright 2022-2023,, 2025 Friedrich von Never");
+        Assert.Equal([
+            new CopyrightNotice.YearItem.YearRange(2022, 2023)
+        ], copyright.Years);
+        Assert.Equal(", 2025 Friedrich von Never", copyright.HolderName);
+    }
+
+    private static async Task<ReuseFileEntry?> ParseFileEntry(string content)
+    {
         var file = Temporary.CreateTempFile();
         try
         {
-            await file.WriteAllTextAsync(fileContent, TestContext.Current.CancellationToken);
-            var content = await ReuseFileEntry.ReadFromFile(file);
-            Assert.Null(content);
+            await file.WriteAllTextAsync(content, TestContext.Current.CancellationToken);
+            return await ReuseFileEntry.ReadFromFile(file);
         }
         finally
         {
             file.Delete();
         }
     }
+
+    private static async Task<CopyrightNotice> ParseCopyrightNotice(string content)
+    {
+        var data = await ParseFileEntry(content);
+        Assert.NotNull(data);
+        return data.CopyrightNotices.Single();
+    }
 }
+// REUSE-IgnoreEnd
